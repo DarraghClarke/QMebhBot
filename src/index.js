f8703a473488d6bdd2d02c99003d754a7f2718f4
DarraghClarke <darragh.clarke@ucdconnect.ie>
2020-05-19 01:24:11 +0100

    updated some permission info, bot now creates a new issue and updates it as the leaderboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  let dict = {}
+  const dict = {}
+  const title = 'QMebh Leaderboard'
+  // eslint-disable-next-line camelcase
+  let issue_number = null
 
   app.log('Yay, the app was loaded!')
 
@@ -13,8 +17,33 @@ module.exports = app => {
   })
 
   app.on('issue_comment.created', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+    const owner = context.payload.sender
+    const repo = context.payload.repository
+    dict[owner] = (dict[owner] || 0) + 1
+    const leaderboardStatus = await leaderboardExists(context)
+    if (leaderboardStatus === true) {
+      await updateLeaderboard(app, context)
+    } else {
+      await createLeaderboard(context)
+    }
   })
 
+  async function updateLeaderboard (app, ctx) {
+    ctx.github.issues.update(ctx.repo({ issue_number, body: ' body update' }))
+  }
+  async function createLeaderboard (ctx) {
+    return ctx.github.issues.create(ctx.repo({ title: title, body: 'new blank leadboderboard' }))
+  }
+  async function leaderboardExists (ctx) {
+    const search = await ctx.github.search.issuesAndPullRequests({
+      q: `${title} in:title repo:${ctx.payload.repository.full_name}`,
+      per_page: 100
+    })
+    if (search.data.total_count !== 0) {
+      // eslint-disable-next-line camelcase
+      issue_number = search.data.items[0].number
+      return true
+    }
+    return false
+  }
 }
