9cb5f8b71d88759c784a47d2aba109b2100aeb6f
DarraghClarke <darragh.clarke@ucdconnect.ie>
2020-05-20 16:22:08 +0100

    updated dependencies to use my fork of metadata, now save leaderboard data to metadata

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+const metadata = require('probot-metadata')
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -18,11 +18,7 @@ module.exports = app => {
 
   app.on('issue_comment.created', async context => {
     const owner = context.payload.sender.login
-    // const login = owner.login
-    // console.log('before')
-    // eslint-disable-next-line no-useless-escape
     console.log(owner)
-    // console.log('after')
     dict[owner] = (dict[owner] || 0) + 1
     const leaderboardStatus = await leaderboardExists(context)
     if (leaderboardStatus === true) {
@@ -34,6 +30,19 @@ module.exports = app => {
   async function updateLeaderboard (app, ctx) {
     const body = await getLeaderBoardRanked()
     ctx.github.issues.update(ctx.repo({ issue_number, body: body.toString() }))
+    await metadata(ctx).set(dict)
+    const dictionary = await metadata(ctx).getAll()
+    // eslint-disable-next-line no-array-constructor
+    var arrayKeys = new Array()
+    // eslint-disable-next-line no-array-constructor
+    var arrayValues = new Array()
+    for (var key in dictionary) {
+      arrayKeys.push(key)
+      arrayValues.push(dictionary[key])
+    }
+
+    console.log(arrayKeys.toString() + ' keys')
+    console.log(arrayValues.toString() + ' values')
   }
   async function createLeaderboard (ctx) {
     return ctx.github.issues.create(ctx.repo({ title: title, body: 'new blank leadboderboard' }))
@@ -50,17 +59,7 @@ module.exports = app => {
     }
     return false
   }
-
-  // eslint-disable-next-line no-unused-vars
-  async function getLeaderboard () {
-    const rankedLeaderboard = getLeaderBoardRanked()
-    return rankedLeaderboard
-  }
-
   async function getLeaderBoardRanked () {
-    // const items = Object.keys(dict).map(function (key) {
-    //  return [key, dict[key]]
-    // })
     // eslint-disable-next-line no-array-constructor
     var arrayKeys = new Array()
     // eslint-disable-next-line no-array-constructor
@@ -72,13 +71,6 @@ module.exports = app => {
 
     console.log(arrayKeys.toString())
     console.log(arrayValues.toString())
-    // Sort the array based on the second element
-    // items.sort(function (first, second) {
-    // return second[1] - first[1]
-    // })
-    // dict.sorte((a, b) => a.value - b.value)
-    // console.log(dict)
-    // return dict.join(', ')
     return arrayKeys.toString().concat(arrayValues.toString())
   }
 }
