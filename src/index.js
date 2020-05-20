faa8f75f97721fc15783b2a60d76a22b11d066c5
DarraghClarke <darragh.clarke@ucdconnect.ie>
2020-05-21 00:54:14 +0100

    minor formatting for leaderboard, not checks if leaderboard exists on launch and then once an hour after that

--- src/index.js
+++ src/index.js
@@ -1,76 +1,90 @@
 const metadata = require('probot-metadata')
+const createScheduler = require('probot-scheduler')
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
-  const dict = {}
+module.exports = bot => {
+  let dict = {}
   const title = 'QMebh Leaderboard'
-  // eslint-disable-next-line camelcase
-  let issue_number = null
+  let issueNumber = null
+  let leaderboardIssue = null
+  bot.log('Yay, the app was loaded!')
 
-  app.log('Yay, the app was loaded!')
-
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  createScheduler(bot, {
+    delay: false
   })
 
-  app.on('issue_comment.created', async context => {
-    const owner = context.payload.sender.login
-    console.log(owner)
-    dict[owner] = (dict[owner] || 0) + 1
-    const leaderboardStatus = await leaderboardExists(context)
-    if (leaderboardStatus === true) {
-      await updateLeaderboard(app, context)
+  bot.on('schedule.repository', context => {
+    // this is called on startup and can access context
+    // if leaderboard exists, gets its info, if it doesn't, make it
+    if (leaderboardExists(context) === true) {
+      getLeaderboardFromMetadata(context)
     } else {
-      await createLeaderboard(context)
+      createLeaderboard(context)
     }
   })
-  async function updateLeaderboard (app, ctx) {
-    const body = await getLeaderBoardRanked()
-    ctx.github.issues.update(ctx.repo({ issue_number, body: body.toString() }))
-    await metadata(ctx).set(dict)
-    const dictionary = await metadata(ctx).getAll()
-    // eslint-disable-next-line no-array-constructor
-    var arrayKeys = new Array()
-    // eslint-disable-next-line no-array-constructor
-    var arrayValues = new Array()
-    for (var key in dictionary) {
-      arrayKeys.push(key)
-      arrayValues.push(dictionary[key])
+
+  bot.on('issues.opened', async context => {
+    const owner = context.payload.sender.login
+    handleUpdate(context, owner, 'issues.opened')
+  })
+
+  bot.on('issue_comment.created', async context => {
+    const owner = context.payload.sender.login
+    handleUpdate(context, owner, 'issue_comment.created')
+  })
+
+  async function handleUpdate (ctx, owner, type) {
+    switch (type) {
+      case 'issue_comment.created':
+        dict[owner] = (dict[owner] || 0) + 1
+        break
+      case 'issues.opened':
+        dict[owner] = (dict[owner] || 0) + 2
+        break
     }
 
-    console.log(arrayKeys.toString() + ' keys')
-    console.log(arrayValues.toString() + ' values')
+    const leaderboardStatus = await leaderboardExists(ctx)
+    if (leaderboardStatus === true) {
+      await updateLeaderboard(bot, ctx)
+    } else {
+      await createLeaderboard(ctx)
+    }
+    await metadata(ctx).set(dict)
   }
+
+  async function updateLeaderboard (bot, ctx) {
+    const body = await getLeaderBoardRanked()
+    ctx.github.issues.update(ctx.repo({ issue_number: issueNumber, body: body.toString() }))
+  }
+
   async function createLeaderboard (ctx) {
     return ctx.github.issues.create(ctx.repo({ title: title, body: 'new blank leadboderboard' }))
   }
+
+  async function getLeaderboardFromMetadata (ctx) {
+    dict = await metadata(ctx, leaderboardIssue).getAll()
+  }
+
   async function leaderboardExists (ctx) {
     const search = await ctx.github.search.issuesAndPullRequests({
       q: `${title} in:title repo:${ctx.payload.repository.full_name}`,
       per_page: 100
     })
     if (search.data.total_count !== 0) {
-      // eslint-disable-next-line camelcase
-      issue_number = search.data.items[0].number
+      leaderboardIssue = search.data.items[0]
+      issueNumber = search.data.items[0].number
       return true
     }
     return false
   }
-  async function getLeaderBoardRanked () {
-    // eslint-disable-next-line no-array-constructor
-    var arrayKeys = new Array()
-    // eslint-disable-next-line no-array-constructor
-    var arrayValues = new Array()
-    for (var key in dict) {
-      arrayKeys.push(key)
-      arrayValues.push(dict[key])
-    }
 
-    console.log(arrayKeys.toString())
-    console.log(arrayValues.toString())
-    return arrayKeys.toString().concat(arrayValues.toString())
+  async function getLeaderBoardRanked () {
+    let formattedLeaderboard = 'Leaderboard\n'
+    for (var key in dict) {
+      formattedLeaderboard = formattedLeaderboard + key + ' - ' + dict[key] + ' points\n'
+    }
+    return formattedLeaderboard
   }
 }
