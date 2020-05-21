5a707660f86daff1259f9ddf7411615d31fa46b3
DarraghClarke <darragh.clarke@ucdconnect.ie>
2020-05-21 19:57:43 +0100

    added config support and listeners for more actions, removed scheduler due to bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const metadata = require('probot-metadata')
-const createScheduler = require('probot-scheduler')
+// const createScheduler = require('probot-scheduler')
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -9,23 +9,14 @@ module.exports = bot => {
   const title = 'QMebh Leaderboard'
   let issueNumber = null
   let leaderboardIssue = null
+  let quietMode = null
+  let createIssuePoints = 2
+  let issueCommentPoints = 1
+  let createPullRequestPoints = 5
+  let pullRequestCommentPoints = 2
+  let pullRequestMergedPoints = 3
+
   bot.log('Yay, the app was loaded!')
-
-  createScheduler(bot, {
-    delay: false
-  })
-
-  bot.on('schedule.repository', context => {
-    // this is called on startup and can access context
-    // if leaderboard exists, gets its info, if it doesn't, make it
-    if (leaderboardExists(context) === true) {
-      getLeaderboardFromMetadata(context)
-    } else {
-      createLeaderboard(context)
-    }
-    // finally update the "database" stored in metadata
-    metadata(context).set(dict)
-  })
 
   bot.on('issues.opened', async context => {
     const owner = context.payload.sender.login
@@ -37,18 +28,51 @@ module.exports = bot => {
     handleUpdate(context, owner, 'issue_comment.created')
   })
 
+  bot.on('pull_request.opened', async context => {
+    const owner = context.payload.sender.login
+    handleUpdate(context, owner, 'pull_request.opened')
+  })
+
+  bot.on('pull_request_review_comment.created', async context => {
+    const owner = context.payload.sender.login
+    handleUpdate(context, owner, 'pull_request_review_comment.created')
+  })
+
+  bot.on('pull_request.merged', async context => {
+    const owner = context.payload.sender.login
+    handleUpdate(context, owner, 'pull_request.merged')
+  })
+
   async function handleUpdate (ctx, owner, type) {
+    if (quietMode === null) {
+      quietMode = true // set to true by default but check incase config says otherwise
+      await loadConfig(ctx)
+      if (await leaderboardExists(ctx) === true) {
+        getLeaderboardFromMetadata(ctx)
+      } else {
+        createLeaderboard(ctx)
+      }
+    }
     switch (type) {
       case 'issue_comment.created':
-        dict[owner] = (dict[owner] || 0) + 1
+        dict[owner] = (dict[owner] || 0) + issueCommentPoints
         break
       case 'issues.opened':
-        dict[owner] = (dict[owner] || 0) + 2
+        dict[owner] = (dict[owner] || 0) + createIssuePoints
+        break
+      case 'pull_request.opened':
+        dict[owner] = (dict[owner] || 0) + createPullRequestPoints
+        break
+      case 'pull_request.merged':
+        dict[owner] = (dict[owner] || 0) + pullRequestMergedPoints
+        break
+      case 'pull_request_review_comment.created':
+        dict[owner] = (dict[owner] || 0) + pullRequestCommentPoints
         break
     }
 
-    const leaderboardStatus = await leaderboardExists(ctx)
-    if (leaderboardStatus === true) {
+    const leaderboardStatus = await leaderboardExists(ctx, ctx.github)
+    if (await leaderboardStatus === true) {
       await updateLeaderboard(bot, ctx)
     } else {
       await createLeaderboard(ctx)
@@ -57,11 +81,19 @@ module.exports = bot => {
 
   async function updateLeaderboard (bot, ctx) {
     const body = await getLeaderBoardRanked()
-    ctx.github.issues.update(ctx.repo({ issue_number: issueNumber, body: body.toString() }))
+    if (quietMode === true || quietMode === null) {
+      console.log('in silent mode, did not make changes, would have updated leaderboard issue with ' + body)
+    } else {
+      ctx.github.issues.update(ctx.repo({ issue_number: issueNumber, body: body.toString() }))
+    }
   }
 
   async function createLeaderboard (ctx) {
-    return ctx.github.issues.create(ctx.repo({ title: title, body: 'new blank leadboderboard' }))
+    if (quietMode === true || quietMode === null) {
+      console.log('in silent mode, did not make changes, would have created leaderboard issue ')
+    } else {
+      ctx.github.issues.create(ctx.repo({ title: title, body: 'new blank leadboderboard' }))
+    }
   }
 
   async function getLeaderboardFromMetadata (ctx) {
@@ -87,5 +119,28 @@ module.exports = bot => {
       formattedLeaderboard = formattedLeaderboard + key + ' - ' + dict[key] + ' points\n'
     }
     return formattedLeaderboard
+  }
+
+  async function loadConfig (context) {
+    const config = await context.config('config.yml')
+    if (config.quietMode === false) {
+      quietMode = config.quietMode
+    }
+
+    if (config.createIssuePoints) {
+      createIssuePoints = config.createIssuePoints
+    }
+    if (config.issueCommentPoints) {
+      issueCommentPoints = config.issueCommentPoints
+    }
+    if (config.createPullRequestPoints) {
+      createPullRequestPoints = config.createPullRequestPoints
+    }
+    if (config.pullRequestCommentPoints) {
+      pullRequestCommentPoints = config.pullRequestCommentPoints
+    }
+    if (config.pullRequestMergedPoints) {
+      pullRequestMergedPoints = config.pullRequestMergedPoints
+    }
   }
 }
